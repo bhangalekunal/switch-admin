2c6cc206bd40f64c5798b02f601efc60e6174398
HP-PAV-15\bhang <bhangalekunal2631996@gmail.com>
2025-04-26 21:48:13 +0530

    added not found component

--- switch-admin-ui/src/app/core/components/not-found/not-found.component.ts
+++ switch-admin-ui/src/app/core/components/not-found/not-found.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'sa-not-found',
-  imports: [],
+  imports: [
+    MatCardModule, 
+    MatButtonModule, 
+    RouterLink, 
+    MatIconModule
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
