2c870f6ae261c573cba6e78067623ce4daae2a5a
HP-PAV-15\bhang <bhangalekunal2631996@gmail.com>
2025-04-26 19:51:28 +0530

    basic routes

--- switch-admin-ui/src/app/app.routes.ts
+++ switch-admin-ui/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
+    },
+    {
+        path: '404',
+        loadComponent: () => import('./core/components/not-found/not-found.component').then(m => m.NotFoundComponent),
+        title: 'Page Not Found'
+    },
+    { path: '**', redirectTo: '404' }
+];
---
+++ switch-admin-ui/src/app/auth/auth.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+export const AUTH_ROUTES: Routes = [
+    { 
+        path: 'login',
+        loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
+        title: 'Login'
+    },
+    { 
+        path: 'forgot-password',
+        loadComponent: () => import('./forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent),
+        title: 'Password Recovery'
+    },
+    { 
+        path: 'reset-password',
+        loadComponent: () => import('./reset-password/reset-password.component').then(m => m.ResetPasswordComponent),
+        title: 'Reset Password'
+    },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
